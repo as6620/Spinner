79c155a84a044412b0ec1c1d5cc70abd78821620
as6620 <as6620@bs.amalnet.k12.il>
2024-11-19 14:09:27 +0200

    adapter

--- app/src/main/java/com/example/spinner/MainActivity.java
+++ app/src/main/java/com/example/spinner/MainActivity.java
@@ -1,17 +1,16 @@
 package com.example.spinner;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     ListView lV;
     Spinner spin;
     TextView tVLastName, tVName, tVBirthDate, tVNumber;
@@ -31,25 +30,78 @@ public class MainActivity extends AppCompatActivity {
     String[] classC_FirstNames = {"Ofer", "Sela", "Yaron", "Halevi", "Natan", "Lavi", "Yitzhak", "Carmi", "Gur", "Barkan"};
     String[] classC_BirthDates = {"12/02/2011", "01/09/2010", "16/05/2009", "21/08/2010", "18/03/2009", "10/12/2008", "04/07/2010", "11/04/2010", "15/11/2010", "09/06/2009"};
     String[] classC_PhoneNumbers = {"0528765432", "0543216547", "0524329876", "0524432211", "0521239876", "0549876543", "0528764321", "0522223333", "0525432111", "0545556667"};
+
+    String[] classD_LastNames = {"Smith", "Johnson", "Williams", "Brown", "Jones", "Miller", "Davis", "Garcia", "Rodriguez", "Wilson"};
+    String[] classD_FirstNames = {"John", "Michael", "Sarah", "Emily", "James", "William", "David", "Jessica", "Daniel", "Samantha"};
+    String[] classD_BirthDates = {"01/01/2010", "02/02/2009", "03/03/2010", "04/04/2008", "05/05/2009", "06/06/2010", "07/07/2008", "08/08/2009", "09/09/2009", "10/10/2009"};
+    String[] classD_PhoneNumbers = {"0521234567", "0547654321", "0522345678", "0548765432", "0523456789", "0524567890", "0545678901", "0526789012", "0527890123", "0548901234"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
 
+        ArrayAdapter<String> adp = new ArrayAdapter<>(this,
+                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, classNames);
+        spin.setAdapter(adp);
 
-
-
-
-
+        spin.setOnItemSelectedListener(this);
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long rowid) {
+        String[] lastNames;
+        String[] firstNames;
+        String[] birthDates;
+        String[] phoneNumbers;
+
+        if (pos == 0) {
+            lastNames = classA_LastNames;
+            firstNames = classA_FirstNames;
+            birthDates = classA_BirthDates;
+            phoneNumbers = classA_PhoneNumbers;
+        } else if (pos == 1) {
+            lastNames = classB_LastNames;
+            firstNames = classB_FirstNames;
+            birthDates = classB_BirthDates;
+            phoneNumbers = classB_PhoneNumbers;
+        } else if (pos == 2) {
+            lastNames = classC_LastNames;
+            firstNames = classC_FirstNames;
+            birthDates = classC_BirthDates;
+            phoneNumbers = classC_PhoneNumbers;
+        } else {
+            lastNames = classD_LastNames;
+            firstNames = classD_FirstNames;
+            birthDates = classD_BirthDates;
+            phoneNumbers = classD_PhoneNumbers;
+        }
+
+        String[] students = new String[lastNames.length];
+        for (int i = 0; i < lastNames.length; i++) {
+            students[i] = firstNames[i] + " " + lastNames[i];
+        }
+
+        lV.setAdapter(new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, students));
+
+        lV.setOnItemClickListener((parent1, view1, pos1, id1) -> {
+            tVLastName.setText("Last Name: " + lastNames[pos1]);
+            tVName.setText("First Name: " + firstNames[pos1]);
+            tVBirthDate.setText("Birth Date: " + birthDates[pos1]);
+            tVNumber.setText("Phone: " + phoneNumbers[pos1]);
+        });
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {}
+
     private void initView() {
-        lV = (ListView) findViewById(R.id.lV);
-        spin = (Spinner) findViewById(R.id.spin);
-        tVLastName = (TextView) findViewById(R.id.tVLastName);
-        tVName = (TextView) findViewById(R.id.tVName);
-        tVBirthDate = (TextView) findViewById(R.id.tVBirthDate);
-        tVNumber = (TextView) findViewById(R.id.tVNumber);
+        lV = findViewById(R.id.lV);
+        spin = findViewById(R.id.spin);
+        tVLastName = findViewById(R.id.tVLastName);
+        tVName = findViewById(R.id.tVName);
+        tVBirthDate = findViewById(R.id.tVBirthDate);
+        tVNumber = findViewById(R.id.tVNumber);
     }
 }
